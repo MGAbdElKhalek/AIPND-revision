5fb7332023f80b99d11e10abf51e47db528da0d6
mohamed.gamaal <mohamed.gamaal>
2020-05-04 01:10:16 +0200

    Fix in TODO 1:
        The "vgg" default input argument should be in lower case

--- intropyproject-classify-pet-images/get_input_args.py
+++ intropyproject-classify-pet-images/get_input_args.py
@@ -44,7 +44,7 @@ def get_input_args():
     
     parser.add_argument('--dir', type = str, default = 'pet_images/', 
                     help = 'path to the folder of pet images')
-    parser.add_argument('--arch', type = str, default = 'VGG', 
+    parser.add_argument('--arch', type = str, default = 'vgg', 
                     help = 'CNN Arch')
     parser.add_argument('--dogfile', type = str, default = 'dognames.txt', 
                     help = 'path to the folder of pet images')
